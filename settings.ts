4faaddb9a94f3202a95ab791b4ff073c22d2478e
Dinhero21 <dinhero21@dinhero21.dev>
2024-10-18 13:27:24 -0300

    lint

--- settings.ts
+++ settings.ts
@@ -1,19 +1,19 @@
-type Level = 'none' | 'warn' | 'error'
+type Level = 'none' | 'warn' | 'error';
 
 interface IncorrectPacketAction {
-  log: Level
-  emit: boolean
+  log: Level;
+  emit: boolean;
 }
 
 interface PacketPrediction {
-  enabled: boolean
-  onIncorrectPacket: IncorrectPacketAction
+  enabled: boolean;
+  onIncorrectPacket: IncorrectPacketAction;
 }
 
 export const PACKET_PREDICTION: PacketPrediction = {
   enabled: true,
   onIncorrectPacket: {
     log: 'warn',
-    emit: false
-  }
-}
+    emit: false,
+  },
+};
--- shared.ts
+++ shared.ts
@@ -1,4 +1,4 @@
-import { type UUID } from 'crypto'
+import { type UUID } from 'crypto';
 
 // https://github.com/PrismarineJS/node-minecraft-protocol/blob/21240f8ab2fd41c76f50b64e3b3a945f50b25b5e/src/index.d.ts#L52
 // export interface ServerPlayerMessage {
@@ -30,29 +30,31 @@ import { type UUID } from 'crypto'
 // ! Aligned with API
 // ! Reconstructed from testing (since API is incomplete)
 export interface ServerPlayerMessage {
-  plainMessage: string
-  unsignedContent: string
-  type: number
-  sender: UUID
+  plainMessage: string;
+  unsignedContent: string;
+  type: number;
+  sender: UUID;
   // no senderTeam (since only pre-1.19)
-  senderName: string
-  targetName: string | undefined
-  verified: boolean
+  senderName: string;
+  targetName: string | undefined;
+  verified: boolean;
 }
 
 export interface ServerSystemMessage {
-  positionId: number
-  formattedMessage: string
+  positionId: number;
+  formattedMessage: string;
 }
 
-export type GlobalMessage = {
-  type: 'player'
-  message: ServerPlayerMessage
-} | {
-  type: 'system'
-  message: ServerSystemMessage
-}
+export type GlobalMessage =
+  | {
+      type: 'player';
+      message: ServerPlayerMessage;
+    }
+  | {
+      type: 'system';
+      message: ServerSystemMessage;
+    };
 
-export type ServerMessage = GlobalMessage['message']
+export type ServerMessage = GlobalMessage['message'];
 
-export type LocalMessage = string
+export type LocalMessage = string;
